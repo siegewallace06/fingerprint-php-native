e0740aa0d1ab30c91f5cc034225144e034a67fa8
Farid Muhammad Eryanto <fbrstudio06@gmail.com>
2023-07-25 21:53:57 +0700

    Add missing column

--- sql_migration/user.sql
+++ sql_migration/user.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS `user` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(255) NOT NULL,
+    `tanggalLahir` DATE NOT NULL,
     `umur` INT NOT NULL,
+    `orangTua` VARCHAR(255) NOT NULL,
     `kelas` VARCHAR(50) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `fullname` VARCHAR(255) NOT NULL
