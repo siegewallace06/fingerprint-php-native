cc1a961de2af5d6555593e46b1695b9eb897d125
farrelfauzan298 <farrel@embreo.global>
2023-07-25 21:07:09 +0700

    add nama orang tua field

--- register/register.php
+++ register/register.php
@@ -10,6 +10,7 @@ if (isset($_POST['submit'])) {
     $fullname = $_POST['fullname'];
     $username = $_POST['username'];
     $umur = $_POST['umur'];
+    $orangTua = $_POST['orangTua'];
     $kelas = $_POST['kelas'];
     $password = $_POST['password'];
 
@@ -18,13 +19,14 @@ if (isset($_POST['submit'])) {
     echo "Username: " . $username . "<br>";
     echo "Kelas: " . $kelas . "<br>";
     echo "Password: " . $password . "<br>";
+    echo "orangTua: " . $orangTua . "<br>";
     echo "umur: " . $password . "<br>";
 
     // // Include Database Connection File
     include_once("../config.php");
 
     // Insert user data into table
-    $result = mysqli_query($mysqli, "INSERT INTO user(username,umur,kelas,password,fullname) VALUES('$username','$umur','$kelas','$password','$fullname')");
+    $result = mysqli_query($mysqli, "INSERT INTO user(username,umur,orangTua,kelas,password,fullname) VALUES('$username','$umur','$orangTua',$kelas','$password','$fullname')");
 
     header("Location: ../index.php");
 }
