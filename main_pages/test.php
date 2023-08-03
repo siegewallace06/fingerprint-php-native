c67e6d98a1b4d16f02fceb7d900ab6d12516b1ac
Farid Muhammad Eryanto <fbrstudio06@gmail.com>
2023-08-03 10:24:03 +0700

    Update Timezone to Jakarta

--- main_pages/test.php
+++ main_pages/test.php
@@ -45,8 +45,8 @@ if (isset($_POST['update_date'])) {
     // Include Database Connection File
     include_once("../config.php");
 
-   // Set the default timezone to Bangkok (UTC + 7)
-    date_default_timezone_set('Asia/Bangkok');
+   // Set the default timezone to Jakarta (UTC + 7)
+    date_default_timezone_set('Asia/Jakarta');
 
     // Get the current date and time in Bangkok time
     $_SESSION['tanggalTes'] = date('Y-m-d');
